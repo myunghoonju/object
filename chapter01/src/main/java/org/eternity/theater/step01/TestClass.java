b2f70e76a5af28eae1601d976db197adf0e9a6aa
JU_MYUNGHOON <myunghoonju@gmail.com>
2020-09-21 00:19:35 +0900

    same class commit (#2)
    Co-authored-by: myunghoonju <myunghoonju@gamil.com>

--- chapter01/src/main/java/org/eternity/theater/step01/TestClass.java
+++ chapter01/src/main/java/org/eternity/theater/step01/TestClass.java
@@ -3,6 +3,13 @@ package org.eternity.theater.step01;
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("myTest : helow World");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
