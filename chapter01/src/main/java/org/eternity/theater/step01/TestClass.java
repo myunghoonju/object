aaf62ad55fd89694a877844442f2da4ee78ab559
myunghoonju <myunghoonju@gamil.com>
2020-09-21 00:13:07 +0900

    same class commit

---
+++ chapter01/src/main/java/org/eternity/theater/step01/TestClass.java
@@ -0,0 +1,15 @@
+package org.eternity.theater.step01;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
